9b36b1dc322389f90a1d97a232f5409adbf44a60
Nikolai Poroshin <mikalai.paroshyn@pandadoc.com>
2021-10-07 15:50:42 +0300

    created new repo and added initial project files

---
+++ conftest.py
@@ -0,0 +1,25 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption("--language", action="store", default="en",
+                     help="browser language")
+
+
+@pytest.fixture
+def browser(request):
+    browser_language = request.config.getoption("language")
+    options = Options()
+    options.add_argument("--window-position=0,0")
+    options.add_experimental_option('prefs', {"intl.accept_languages": browser_language})
+    if browser_language:
+        print(f"\nusing browser language: {browser_language}")
+        browser = webdriver.Chrome(options=options)
+        browser.maximize_window()
+    else:
+        raise pytest.UsageError("missing required parameter: --language")
+    yield browser
+    print("\nbrowser quit")
+    browser.quit()
