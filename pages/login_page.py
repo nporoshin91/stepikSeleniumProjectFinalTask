900c8dc0295ff6072ac7854597f070b74443df4b
Nikolai Poroshin <mikalai.paroshyn@pandadoc.com>
2021-10-08 21:11:19 +0300

    fixed method name in base_page and fixed assert in login_page

--- pages/login_page.py
+++ pages/login_page.py
@@ -9,7 +9,7 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        assert "login" in self.current_url()
+        assert "login" in self.current_url(), "Current URL is not login URL"
 
     def should_be_login_form(self):
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is missing"
