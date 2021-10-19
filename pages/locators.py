142f536e924c6407a2229cc4dc00ded0e626d255
Nikolai Poroshin <mikalai.paroshyn@pandadoc.com>
2021-10-20 01:06:28 +0300

    added class TestUserAddToBasketFromProductPage and setup function

--- pages/locators.py
+++ pages/locators.py
@@ -4,6 +4,10 @@ from selenium.webdriver.common.by import By
 class LoginPageLocators:
     LOGIN_FORM = (By.ID, "login_form")
     REGISTER_FORM = (By.ID, "register_form")
+    REGISTER_FORM_EMAIL = (By.ID, "id_registration-email")
+    REGISTER_FORM_PASS = (By.ID, "id_registration-password1")
+    REGISTER_FORM_PASS_CONFIRM = (By.ID, "id_registration-password2")
+    REGISTER_FORM_REGISTER_BTN = (By.NAME, "registration_submit")
 
 
 class ProductPageLocators:
@@ -19,3 +23,4 @@ class BasePageLocators:
     CART_LINK = (By.CSS_SELECTOR, ".btn-group")
     CART_ITEM = (By.ID, "basket_formset")
     CART_EMPTY = (By.XPATH, "//div[@id='content_inner']/p[contains(text(), 'Your basket is empty')]")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
--- test_product_page.py
+++ test_product_page.py
@@ -1,55 +1,10 @@
+import uuid
+
 import pytest
 
 from pages.basket_page import BasketPage
+from pages.login_page import LoginPage
 from pages.product_page import ProductPage
-
-
-@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-                                  pytest.param("bugged_link", marks=pytest.mark.xfail),
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
-def test_guest_can_add_product_to_basket(browser, link):
-    page = ProductPage(browser, link)
-    page.open()
-    page.should_be_add_to_basket_button()
-    page.click_on_add_to_basket_button()
-    page.solve_quiz_and_get_code()
-    page.should_be_success_messages()
-    goods_name = page.get_goods_name()
-    goods_price = page.get_goods_price()
-    success_messages = page.get_success_messages()  # тот самый список с трёмя "зелёными" сообщениями
-
-    # переменная необходима для того, чтобы выйти из цикла поиска сразу после нахождения совпадения и сохранить рез-тат
-    item_in_message = False
-
-    # пробуем найти название товара среди "зелёных" сообщений
-    for message in success_messages:
-        if goods_name == message:
-            item_in_message = True
-            break
-        else:
-            item_in_message = False
-    if not item_in_message:
-        assert False, "Success message doesn't contain goods name"
-
-    # пробуем найти цену товара среди "зелёных" сообщений
-    for message in success_messages:
-        if goods_price == message:
-            item_in_message = True
-            break
-        else:
-            item_in_message = False
-    if not item_in_message:
-        assert False, "Success message doesn't contain goods price"
-
-    page.should_not_be_success_message()
-
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
 
@@ -60,12 +15,6 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     page.open()
     page.should_be_add_to_basket_button()
     page.click_on_add_to_basket_button()
-    page.should_not_be_success_message()
-
-
-def test_guest_cant_see_success_message(browser):
-    page = ProductPage(browser, link)
-    page.open()
     page.should_not_be_success_message()
 
 
@@ -100,3 +49,57 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page = BasketPage(browser, link)
     page.should_not_be_cart_items()
     page.should_be_empty_cart_message()
+
+
+@pytest.mark.users
+class TestUserAddToBasketFromProductPage:
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        self.link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+        self.email = f"{str(uuid.uuid4().hex)}@stepikcoursetest.org"
+        self.password = str(uuid.uuid4().hex)
+        self.page = LoginPage(browser, self.link)
+        self.page.open()
+        self.page.register_new_user(self.email, self.password)
+        self.page.should_be_authorized_user()
+
+    def test_user_cant_see_success_message(self, browser):
+        self.link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/hacking-exposed-wireless_206/"
+        page = ProductPage(browser, self.link)
+        page.open()
+        page.should_not_be_success_message()
+
+    def test_user_can_add_product_to_basket(self, browser):
+        self.link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/?promo=offer0"
+        page = ProductPage(browser, self.link)
+        page.open()
+        page.should_be_add_to_basket_button()
+        page.click_on_add_to_basket_button()
+        page.solve_quiz_and_get_code()
+        page.should_be_success_messages()
+        goods_name = page.get_goods_name()
+        goods_price = page.get_goods_price()
+        success_messages = page.get_success_messages()  # тот самый список с трёмя "зелёными" сообщениями
+
+        # переменная необходима для того, чтобы выйти из цикла поиска сразу после нахождения совпадения и сохранить рез-тат
+        item_in_message = False
+
+        # пробуем найти название товара среди "зелёных" сообщений
+        for message in success_messages:
+            if goods_name == message:
+                item_in_message = True
+                break
+            else:
+                item_in_message = False
+        if not item_in_message:
+            assert False, "Success message doesn't contain goods name"
+
+        # пробуем найти цену товара среди "зелёных" сообщений
+        for message in success_messages:
+            if goods_price == message:
+                item_in_message = True
+                break
+            else:
+                item_in_message = False
+        if not item_in_message:
+            assert False, "Success message doesn't contain goods price"
