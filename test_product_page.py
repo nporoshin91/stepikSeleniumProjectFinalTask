b4db1b9aeb3f16a0dac2cfeccd222a577e891a0f
Nikolai Poroshin <mikalai.paroshyn@pandadoc.com>
2021-10-20 10:59:30 +0300

    getting ready for review

--- test_product_page.py
+++ test_product_page.py
@@ -36,12 +36,14 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
 
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
@@ -51,7 +53,45 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page.should_be_empty_cart_message()
 
 
-@pytest.mark.users
+link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/?promo=offer0"
+
+
+@pytest.mark.need_review
+def test_guest_can_add_product_to_basket(browser):
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_add_to_basket_button()
+    page.click_on_add_to_basket_button()
+    page.solve_quiz_and_get_code()
+    page.should_be_success_messages()
+    goods_name = page.get_goods_name()
+    goods_price = page.get_goods_price()
+    success_messages = page.get_success_messages()  # тот самый список с трёмя "зелёными" сообщениями
+
+    # переменная необходима для того, чтобы выйти из цикла поиска сразу после нахождения совпадения и сохранить рез-тат
+    item_in_message = False
+
+    # пробуем найти название товара среди "зелёных" сообщений
+    for message in success_messages:
+        if goods_name == message:
+            item_in_message = True
+            break
+        else:
+            item_in_message = False
+    if not item_in_message:
+        assert False, "Success message doesn't contain goods name"
+
+    # пробуем найти цену товара среди "зелёных" сообщений
+    for message in success_messages:
+        if goods_price == message:
+            item_in_message = True
+            break
+        else:
+            item_in_message = False
+    if not item_in_message:
+        assert False, "Success message doesn't contain goods price"
+
+
 class TestUserAddToBasketFromProductPage:
     @pytest.fixture(scope="function", autouse=True)
     def setup(self, browser):
@@ -69,6 +109,7 @@ class TestUserAddToBasketFromProductPage:
         page.open()
         page.should_not_be_success_message()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         self.link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/?promo=offer0"
         page = ProductPage(browser, self.link)
